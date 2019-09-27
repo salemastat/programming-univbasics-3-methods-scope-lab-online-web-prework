6153185423b27acafa3bb29b2795dfcf13830336
Salem Asrat <salemasrat@gmail.com>
2019-09-27 17:03:56 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -1,6 +1,6 @@
 describe "any_phrase" do
   it "puts out 'Do A Barrel Roll!'" do
     phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
+    expect{phrase}.to output("Do A Barrel Roll!\n").to_stdout
   end
 end
